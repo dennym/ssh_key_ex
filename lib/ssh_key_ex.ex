2f37a08c8f260dd59aae15f9480d0f9d9985081d
Denny Mueller <contact@dennymueller.io>
2025-03-11 19:42:35 +0100

    initial commit

---
+++ lib/ssh_key_ex.ex
@@ -0,0 +1,18 @@
+defmodule SshKeyEx do
+  @moduledoc """
+  Documentation for `SshKeyEx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SshKeyEx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ssh_key_ex_test.exs
@@ -0,0 +1,8 @@
+defmodule SshKeyExTest do
+  use ExUnit.Case
+  doctest SshKeyEx
+
+  test "greets the world" do
+    assert SshKeyEx.hello() == :world
+  end
+end
