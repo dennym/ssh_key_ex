9302f36f3d117f82043bbd9f2328a675f199aa83
Denny Mueller <contact@dennymueller.io>
2025-03-12 19:36:00 +0100

    add more key algorithms

--- test/ssh_key_ex/public_key_test.exs
+++ test/ssh_key_ex/public_key_test.exs
@@ -19,4 +19,30 @@ defmodule SSHKeyEx.PublicKeyTest do
     assert %{comment: "cup@saucer.com"} = key_information
     assert %{algorithm: "ssh-rsa"} = key_information
   end
+
+  test "parses ecdsa-sha2-nistp256 public key into PublicKey struct" do
+    certificate = File.read!("test/support/id_ecdsa256.pub")
+    {:ok, key_information} = PublicKey.from_openssh(certificate)
+
+    assert %{comment: "cup@saucer.com"} = key_information
+    assert %{algorithm: "ecdsa-sha2-nistp256"} = key_information
+  end
+
+  test "parses ecdsa-sha2-nistp384 public key into PublicKey struct" do
+    certificate = File.read!("test/support/id_ecdsa384.pub")
+    {:ok, key_information} = PublicKey.from_openssh(certificate)
+
+    assert %{comment: "cup@saucer.com"} = key_information
+    assert %{algorithm: "ecdsa-sha2-nistp384"} = key_information
+  end
+
+  test "parses ecdsa-sha2-nistp521 public key into PublicKey struct" do
+    certificate = File.read!("test/support/id_ecdsa521.pub")
+    {:ok, key_information} = PublicKey.from_openssh(certificate)
+
+    assert %{comment: "cup@saucer.com"} = key_information
+    assert %{algorithm: "ecdsa-sha2-nistp521"} = key_information
+  end
+
+  #  `ssh-dss`
 end
