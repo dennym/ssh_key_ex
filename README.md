97d23cf4d90c3681545e4ef28fd9bf3c2464ca47
Denny Mueller <contact@dennymueller.io>
2025-03-29 02:20:57 +0100

    use rustler precompiled and bump version

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ by adding `ssh_key_ex` to your list of dependencies in `mix.exs`:
 ```elixir
 def deps do
   [
-    {:ssh_key_ex, "~> 0.2.0"}
+    {:ssh_key_ex, "~> 0.3.0"}
   ]
 end
 ```
--- lib/ssh_key_ex/native.ex
+++ lib/ssh_key_ex/native.ex
@@ -3,7 +3,16 @@ defmodule SSHKeyEx.Native do
   Documentation for `SSHKeyEx.Native`.
   """
 
-  use Rustler, otp_app: :ssh_key_ex, crate: "ssh_key_nif"
+  version = Mix.Project.config()[:version]
+
+  use RustlerPrecompiled,
+    otp_app: :ssh_key_ex,
+    crate: "ssh_key_nif",
+    base_url: "https://github.com/dennym/ssh_key_ex/releases/download/v#{version}",
+    force_build: System.get_env("RUSTLER_FORCE_BUILD") in ["1", "true"],
+    targets:
+      Enum.uniq(["aarch64-unknown-linux-musl" | RustlerPrecompiled.Config.default_targets()]),
+    version: version
 
   defstruct algorithm: "", comment: "", encrypted: false
 
