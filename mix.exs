f6829afdbd4730b1126815c53fcdc4e2d5c055b4
Denny Mueller <contact@dennymueller.io>
2025-03-31 09:16:34 +0200

    add checksum files to be able to publish

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule SshKeyEx.MixProject do
 
   defp package do
     [
-      files: ["lib", "native", "mix.exs", "README.md", "LICENSE", "VERSION"],
+      files: ["lib", "native", "mix.exs", "README.md", "LICENSE", "VERSION", "checksum-*.exs"],
       maintainers: ["Denny Mueller"],
       licenses: ["Apache-2.0"],
       links: %{
